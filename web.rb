05d42074c8f00c074ebe387d951b189a0508332d
Bryce Hamrick <brycehamrick@gmail.com>
2014-07-31 12:31:29 -0700

    working metrics

---
+++ web.rb
@@ -0,0 +1,30 @@
+require 'rubygems'
+require 'sinatra'
+require 'pivotal-tracker'
+
+enable :sessions
+PivotalTracker::Client.use_ssl = true
+
+get '/' do
+  if !session[:token].nil?
+    PivotalTracker::Client.token = session[:token]
+    projects = PivotalTracker::Project.all
+    session[:projects] = projects
+    redirect "/project/#{projects[0].id}"
+  else
+    erb :signin
+  end
+end
+
+post '/signin' do
+  token = PivotalTracker::Client.token(params[:username], params[:password])
+  session[:token] = token
+  redirect '/'
+end
+
+get '/project/:project' do |pid|
+  PivotalTracker::Client.token = session[:token]
+  project = PivotalTracker::Project.find(pid)
+  stories = project.stories.all(:story_type => ['feature'], :current_state => ['unstarted', 'accepted', 'started', 'rejected'], :includedone => 'true')
+  erb :projects, :locals => { :projects => session[:projects], :stories => stories }
+end
